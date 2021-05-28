f7f7ab647b4d6c32fb56587900c1520eb8d08cdb
Chigusa-Sato <chigusa.sato@rakus-partners.co.jp>
2021-05-28 12:19:01 +0900

    'firebaseからユーザー登録情報を取得しました'

--- src/components/cart/Order.js
+++ src/components/cart/Order.js
@@ -1,72 +1,82 @@
-import React, {Component,useState} from "react";
+import React, { Component, useState } from "react";
 import { connect } from "react-redux";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { OrderComp } from "../../views/OrderComp";
 import { db } from "../../firebase/index";
 
 export const Order = () => {
-  const [ordername, setName]=useState('');
-  const [orderemail, setEmail]=useState('');
-  const [orderzip, setZip]=useState('');
-  const [orderaddress, setAddress]=useState('');
-  const [ordertel, setTel]=useState('');
-  const [orderdate,setDate]=useState('');
-  const [ordercredit,setCredit]=useState('');
-  const [isError, setIsError]=useState(false);//falseならエラーなし、trueならエラー
+  const [ordername, setName] = useState("");
+  const [orderemail, setEmail] = useState("");
+  const [orderzip, setZip] = useState("");
+  const [orderaddress, setAddress] = useState("");
+  const [ordertel, setTel] = useState("");
+  const [orderdate, setDate] = useState("");
+  const [ordercredit, setCredit] = useState("");
+  const [isError, setIsError] = useState(false); //falseならエラーなし、trueならエラー
 
-  const checkName=(name)=>{
-    setName(name.target.value);//入力値を取得
-    console.log(ordername);//stateに入力値が代入された
-    if(!ordername){
+  const checkName = (name) => {
+    setName(name.target.value); //入力値を取得
+    console.log(ordername); //stateに入力値が代入された
+    if (!ordername) {
       setIsError(true);
-    }else{
+    } else {
       setIsError(false);
     }
-  }
+  };
 
-  const checkMail=(email)=>{//第二引数にコールバック？？？
+  const checkMail = (email) => {
+    //第二引数にコールバック？？？
     setEmail(email.target.value);
     console.log(orderemail);
-    if(!orderemail){
+    if (!orderemail) {
       setIsError(true);
-    }else{
+    } else {
       setIsError(false);
     }
-  }
-  const checkZip=(zip)=>{
+  };
+  const checkZip = (zip) => {
     setZip(zip.target.value);
     console.log(orderzip);
-  }
-  const checkAddress=(address)=>{
+  };
+  const checkAddress = (address) => {
     setAddress(address.target.value);
     console.log(orderaddress);
-
-  }
-  const checkTel=(tel)=>{
+  };
+  const checkTel = (tel) => {
     setTel(tel.target.value);
     console.log(ordertel);
-  }
-  const checkDate=(date)=>{
+  };
+  const checkDate = (date) => {
     setDate(date.target.value);
-    console.log(orderdate)
-  }
-  const checkCard=(credit)=>{
+    console.log(orderdate);
+  };
+  const checkCard = (credit) => {
     setCredit(credit.target.value);
     console.log(ordercredit);
-  }
-  
-const getUserInfo = ()=>{
-  console.log("getUserInfo呼び出し");
-    db.collection("users/${:userId}/userinfo").get().then((res)=>{
-      console.log(res);
-    })
-};
+  };
+
+  const getUserInfo = () => {
+    console.log("getUserInfo呼び出し");
+    let userdata = {};
+    db.collection(`users/Qk4xMuaeBuMccnx1gEb7fx207ah2/userInfo`)
+      .get()
+      .then((res) => {
+        userdata = res;
+        console.log(userdata);
+        // snapShot.forEach((doc)=>{
+        //   userdata.push(
+        //     doc.data());
+        // });
+        // console.log(userdata)
+      });
+  };
 
   return (
     <React.Fragment>
       <Router>
         <div>
-          <button onClick={getUserInfo}>consoleで確認</button><br/>
+          <button onClick={getUserInfo}>consoleで確認</button>
+          <br />
           注文確認画面です。
           <form>
             <div>
@@ -78,7 +88,8 @@ const getUserInfo = ()=>{
                       <div>お名前</div>
                     </td>
                     <td>
-                      <input type="text" id='name' onChange={checkName}/>{isError &&<p> 名前を入力してください</p>}
+                      <input type="text" id="name" onChange={checkName} />
+                      {isError && <p> 名前を入力してください</p>}
                     </td>
                   </tr>
                   <tr>
@@ -86,7 +97,8 @@ const getUserInfo = ()=>{
                       <div>メールアドレス</div>
                     </td>
                     <td>
-                      <input type="text" id='mail' onChange={checkMail}/>{isError &&<p> メールアドレスを入力してください</p>}
+                      <input type="text" id="mail" onChange={checkMail} />
+                      {isError && <p> メールアドレスを入力してください</p>}
                     </td>
                   </tr>
                   <tr>
@@ -94,7 +106,8 @@ const getUserInfo = ()=>{
                       <div>郵便番号</div>
                     </td>
                     <td>
-                      <input type="text" onChange={checkZip}></input>&nbsp;&nbsp;
+                      <input type="text" onChange={checkZip}></input>
+                      &nbsp;&nbsp;
                       <button>住所検索</button>
                     </td>
                   </tr>
@@ -103,7 +116,7 @@ const getUserInfo = ()=>{
                       <div>住所</div>
                     </td>
                     <td>
-                      <input type="text" onChange={checkAddress}/>
+                      <input type="text" onChange={checkAddress} />
                     </td>
                   </tr>
                   <tr>
@@ -111,7 +124,7 @@ const getUserInfo = ()=>{
                       <div>電話番号</div>
                     </td>
                     <td>
-                      <input type="text" onChange={checkTel}/>
+                      <input type="text" onChange={checkTel} />
                     </td>
                   </tr>
                   <tr>
@@ -119,31 +132,31 @@ const getUserInfo = ()=>{
                       <div>配達日時</div>
                     </td>
                     <td>
-                      <input type="date" onChange={checkDate}/>
+                      <input type="date" onChange={checkDate} />
                     </td>
                   </tr>
                   <tr>
                     <td></td>
                     <td>
-                      <input type="radio" name="time" velue="10"/>
+                      <input type="radio" name="time" velue="10" />
                       10時
-                      <input type="radio" name="time" velue="11"/>
+                      <input type="radio" name="time" velue="11" />
                       11時
-                      <input type="radio" name="time" velue="12"/>
+                      <input type="radio" name="time" velue="12" />
                       12時
-                      <input type="radio" name="time" velue="13"/>
+                      <input type="radio" name="time" velue="13" />
                       13時
-                      <input type="radio" name="time" velue="14"/>
+                      <input type="radio" name="time" velue="14" />
                       14時
                     </td>
                     <td>
-                      <input type="radio" name="time" velue="15"/>
+                      <input type="radio" name="time" velue="15" />
                       15時
-                      <input type="radio" name="time" velue="16"/>
+                      <input type="radio" name="time" velue="16" />
                       16時
-                      <input type="radio" name="time" velue="17"/>
+                      <input type="radio" name="time" velue="17" />
                       17時
-                      <input type="radio" name="time" velue="18"/>
+                      <input type="radio" name="time" velue="18" />
                       18時
                     </td>
                   </tr>
@@ -153,8 +166,14 @@ const getUserInfo = ()=>{
                     </td>
                     <td>
                       <label>
-                        <input type="radio" name="pay" velue="cash"></input>代金引換
-                        <input type="radio" name="pay" velue="creditcard"></input>クレジットカード決済
+                        <input type="radio" name="pay" velue="cash"></input>
+                        代金引換
+                        <input
+                          type="radio"
+                          name="pay"
+                          velue="creditcard"
+                        ></input>
+                        クレジットカード決済
                       </label>
                     </td>
                   </tr>
