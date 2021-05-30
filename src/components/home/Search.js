b17e3a22ca87dee057c2729d125b901478fafc6e
南博之伸 <minamihironoshin@minamihiroyukishinnoMacBook-Pro.local>
2021-05-30 21:29:18 +0900

    動的処理の編集

--- src/components/home/Search.js
+++ src/components/home/Search.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect}  from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchitems } from "../../actions"
+import  Box  from '@material-ui/core/Box';
+import { TextField } from "@material-ui/core";
 
 
 export const Search = (props) => {
   
   const [keyword, setKeyword] = useState();
+  const [error, setError] = useState('')
   const items = useSelector(state => state.items)
   const dispatch = useDispatch()
   useEffect(() => { 
@@ -13,43 +16,52 @@ export const Search = (props) => {
   }, [])
 
 
-  const searchKeyword  = (word) => {
+
+  const searchKeyword  = () => {
     console.log(items)
     let array = []
-    props.itemsData.forEach(item => {if(item.name.indexOf(word) >= 0){
+    props.itemsData.forEach(item => {if(item.name.indexOf(keyword) >= 0){
       array.push(item)
     }}) 
-    if(word === ""){
+    if(keyword === ""){
       console.log(items)
       props.setItemsData(items)
+      setError()
+    }else if(array.length === 0){
+      setError("※該当する商品はありません")
+      props.setItemsData(array)
     }else{
       console.log(items)
       props.setItemsData(array)
+      setError()
     }
   }  
 
 
   return (
     <React.Fragment>
-    <div class="row">
-			<div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-sm-10 col-xs-12">
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<div class="panel-title">商品を検索する</div>
-					</div>
-					<div class="panel-body">
-							<div class="form-group">
-								<label for="code" class="control-label col-sm-2">商品名</label>
-								<div class="col-sm-9">
-									<input type="text"  name="code" id="code" class="form-control input-sm"/>
-                  <button value="検索" class="btn btn-primary" onClick={(e) => searchKeyword(e.target.previousElementSibling.value)} >検索</button>
-								  <button type="reset" value="クリア" class="btn btn-default" onClick={()=>setKeyword("")}>クリア</button>
-								</div>
-							</div>
-					</div>
-				</div>
-			</div>
-		</div>
+    <Box width="100%">
+			<Box ml="16.66666667%" width= "66.66666667%">
+				<Box border={1} borderColor="#ddd"  borderRadius={4} mb={2.5} >
+					<Box bgcolor="#f5f5f5" py={1.25} px={1.875} >
+						<Box fontSize={16}>商品を検索する</Box>
+					</Box>
+					<Box p={1.875} fontSize={14}>
+							<Box>
+                <Box mx="auto" mb={1.875} width="75%" >
+                  <label for="code" class="control-label col-sm-2" >商品名</label>
+                  <TextField label="キーワードを入力" variant="outlined" py={1.25} px={1.875} fullWidth type="text" value={keyword} name="code" id="code" class="form-control input-sm" onChange={(e) => setKeyword(e.target.value)} /><br />
+                </Box>
+                <Box mx="auto" width="100%">
+                {error && <p>{error}</p>}
+                  <button value="検索" class="btn btn-primary" onClick={searchKeyword} >検索</button>
+                  <button type="reset" value="クリア" class="btn btn-default" onClick={()=>setKeyword("")}>クリア</button>
+                </Box>
+							</Box>
+					</Box>
+				</Box>
+			</Box>
+		</Box>
     </React.Fragment>
   );
 }
