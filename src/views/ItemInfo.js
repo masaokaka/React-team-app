b785d0c4a62098b0017dd18a86ea0dea8d3050af
OkawaraTakumi <takumi.okawara@rakus-partners.co.jp>
2021-05-30 18:18:58 +0900

    商品詳細の機能を追加しました

--- src/views/ItemInfo.js
+++ src/views/ItemInfo.js
@@ -18,6 +18,8 @@ export const ItemInfo = () => {
   const toppings = useSelector(state => state.toppings);
   const [itemRendering, setItemRendering] = useState('');
   const [toppingsRendering,setToppings] = useState([]);
+  const [toppingFlag,setToppingFlag] = useState({});
+
   useEffect(()=>{
       items.forEach(item => {
       if(`${item.id}` === itemid){
@@ -30,18 +32,66 @@ export const ItemInfo = () => {
     setToppings(toppings);
   },[toppings])
   
+  //トッピングの数量選択欄の表示切り替え
+  useEffect(() => {
+    const newToppingFlag = {}
+    toppingsRendering.forEach((topping) => {
+      newToppingFlag[topping.name] = false;
+    })
+    setToppingFlag(newToppingFlag);
+  },[toppingsRendering])
+
+  const changeFlag = (e) => {
+    if(toppingFlag[e.target.name] === false ){
+      setToppingFlag({...toppingFlag,[e.target.name]:true})
+    } else {
+      //チェックボックスを閉じた時にトッピングの数は0になる
+      const newToppingSize = {}
+      toppingsRendering.forEach((topping) => {
+      newToppingSize[topping.name] = 0;
+    })
+    setCalcToppingSize(newToppingSize);
+      setToppingFlag({...toppingFlag,[e.target.name]:false})
+    }
+  }
+
+  //トッピングのサイズを取得
+  const [calcToppingSize,setCalcToppingSize] = useState({})
+
+  useEffect (() => {
+    const newToppingSize = {}
+    toppingsRendering.forEach((topping) => {
+      newToppingSize[topping.name] = 0;
+    })
+    setCalcToppingSize(newToppingSize);
+  },[toppingsRendering])
+//トッピングのサイズを切り替える
+  const changeToppingSize = (e) =>{
+    if(e.target.value === '1'){
+      setCalcToppingSize((prevSize) => {
+        return {...prevSize,[e.target.name]:1}
+      })
+    }else if(e.target.value === '2'){
+      setCalcToppingSize((prevSize) => {
+        return {...prevSize,[e.target.name]:2}
+      })
+    }
+  }
+
+
   const [selectValue, setSelectValue] = useState(1)
+  //セレクトボックスの値を取得
   const getSelectValue = (e) => {
     setSelectValue(e.target.value);
   }
 
   const [totalPrice,setTotalPrice] = useState(0)
   const [sizeValue,setSizeValue] = useState(1)
-  const setSize1 = (e) => {
+  const setSize1 = () => {
     setSizeValue(1)
   }
 
-  const setSize2 = (e) => {
+  const setSize2 = () => {
     setSizeValue(2)
   }
 const[calcPrice,setCalcPrice] = useState(0)
@@ -57,17 +107,27 @@ const[calcPrice,setCalcPrice] = useState(0)
     }
   },[sizeValue])
 
-  useEffect(() => {
-    let calculation = calcPrice * selectValue; 
+  //表示合計金額の計算
+  useEffect(() => { 
+    let toppingObjValues = Object.values(calcToppingSize)
+    let mCount = 0;
+    let lCount = 0;
+    toppingObjValues.forEach((value) => {
+          if(value === 1){
+            mCount += 1; 
+          }else if (value === 2){
+            lCount += 1;
+          }
+        })
+    let calculation = calcPrice * selectValue +(mCount * 200) + (lCount * 300); 
     setTotalPrice(calculation)
-  },[selectValue,calcPrice])
-
+  },[selectValue,calcPrice,calcToppingSize])
 
 
   return (
     <React.Fragment>
-      <div class="container">
-        <nav class="navbar navbar-default">
+      <div className="container">
+        {/* <nav class="navbar navbar-default">
         <div class="container-fluid">
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -87,68 +147,71 @@ const[calcPrice,setCalcPrice] = useState(0)
             </p>
           </div>
         </div>
-      </nav>
+      </nav> */}
 
       
-        <div class="row">
-          <div class="col-xs-offset-2 col-xs-8">
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
 
-            <h3 class="text-center">商品詳細</h3>
-            <div class="row">
-              <div class="col-xs-5">
-                <img src={itemRendering.img} class="img-responsive img-rounded item-img-center" />
+            <h3 className="text-center">商品詳細</h3>
+            <div className="row">
+              <div className="col-xs-5">
+                <img src={itemRendering.img} className="img-responsive img-rounded item-img-center" />
               </div>
-
-              <div class="col-xs-5">
-                <div class="bs-component">
+              
+              <div className="col-xs-5">
+                <div className="bs-component">
                   <h4>{itemRendering.name}</h4> <br/>
                   <br/>
                   <p>{itemRendering.text}</p>
                 </div>
               </div>
             </div><br/>
-            <div class="row">
-              <div class="col-xs-offset-2 col-xs-8">
-                <div class="form-group">
-                  <div class="row">
-                    <div class="col-sm-12">
-                      <label for="inputResponsibleCompany">サイズ</label>
+            <div className="row">
+              <div className="col-xs-offset-2 col-xs-8">
+                <div className="form-group">
+                  <div className="row">
+                    <div className="col-sm-12">
+                      <label htmlFor="inputResponsibleCompany">サイズ</label>
                     </div>
-                    <div class="col-sm-12">
-                      <label class="radio-inline">
+                    <div className="col-sm-12">
+                      <label className="radio-inline">
                         <input type="radio" name="responsibleCompany" checked={sizeValue === 1} onChange={(e) => {setSize1(e)}}/>
-                        <span class="price">&nbsp;М&nbsp;</span>&nbsp;&nbsp;{itemRendering.mprice}(税抜)
+                        <span className="price">&nbsp;М&nbsp;</span>&nbsp;&nbsp;{itemRendering.mprice}(税抜)
                       </label>
-                      <label class="radio-inline">
+                      <label className="radio-inline">
                         <input type="radio" name="responsibleCompany" checked={sizeValue === 2} onChange={(e) => {setSize2(e)}}/>
-                        <span class="price">&nbsp;Ｌ</span>&nbsp;&nbsp;{itemRendering.lprice}(税抜)
+                        <span className="price">&nbsp;Ｌ</span>&nbsp;&nbsp;{itemRendering.lprice}(税抜)
                       </label>
                     </div>
                   </div>
                 </div>
               </div>
             </div><br/>
-            <div class="row">
-              <div class="col-xs-offset-2 col-xs-8">
-                <div class="form-group">
-                  <div class="row">
-                    <div class="col-sm-12">
-                      <label for="inputResponsibleCompany">
+            <div className="row">
+              <div className="col-xs-offset-2 col-xs-8">
+                <div className="form-group">
+                  <div className="row">
+                    <div className="col-sm-12">
+                      <label htmlFor="inputResponsibleCompany">
                         トッピング：&nbsp;1つにつき
                         <span>&nbsp;М&nbsp;</span>&nbsp;&nbsp;200円(税抜)
                         <span>&nbsp;Ｌ</span>&nbsp;&nbsp;300円(税抜)
                       </label>
                     </div>
-                    <div class="col-sm-12">
+                    <div className="col-sm-12">
                       {toppingsRendering.map((topping,index) => {
                         return (
-                          <label class="checkbox-inline" key={index}>
-                          <input type="checkbox" />{topping.name}
-                              <span style={{display:true}}>
-                                  <input type="radio" value='1' name='size'/>
-                                  <input type="radio" value='2' name='size'/>
-                              </span>
-                        </label>
+                          <div key={index}>
+                             <label className="checkbox-inline">
+                                  <input name={toppingsRendering[index].name} type="checkbox"  onChange={(e) => {changeFlag(e)}}/>{topping.name}
+                                  <span style={{display:toppingFlag[topping.name] ? 'block':'none'}}>
+                                    M<input type="radio" value='1' name={toppingsRendering[index].name} checked={calcToppingSize[toppingsRendering[index].name] === 1} onChange={(e) => {changeToppingSize(e)}}/>
+                                    L<input type="radio" value='2' name={toppingsRendering[index].name} checked={calcToppingSize[toppingsRendering[index].name] === 2} onChange={(e) => {changeToppingSize(e)}}/>
+                                  </span>
+                             </label>
+                          </div>
+                         
                         )
                       })}
                     </div>
@@ -156,14 +219,14 @@ const[calcPrice,setCalcPrice] = useState(0)
                 </div>
               </div>
             </div>
-            <div class="row">
-              <div class="col-xs-offset-2 col-xs-8">
-                <div class="form-group">
-                  <div class="row">
-                    <div class="col-xs-5 col-sm-5">
-                      <label for="">数量:</label>
-                      <label class="control-label" for="inputError">数量を選択してください</label> 
-                      <select name="area" class="form-control" onChange={(e)=>{ getSelectValue(e) }}>
+            <div className="row">
+              <div className="col-xs-offset-2 col-xs-8">
+                <div className="form-group">
+                  <div className="row">
+                    <div className="col-xs-5 col-sm-5">
+                      <label htmlFor="">数量:</label>
+                      <label className="control-label" htmlFor="inputError">数量を選択してください</label> 
+                      <select className="area" className="form-control" onChange={(e)=>{ getSelectValue(e) }}>
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
@@ -183,18 +246,18 @@ const[calcPrice,setCalcPrice] = useState(0)
               </div>
             </div>
             <br/>
-            <div class="row">
-              <div class="col-xs-offset-2 col-xs-10">
-                <div class="form-group">
+            <div className="row">
+              <div className="col-xs-offset-2 col-xs-10">
+                <div className="form-group">
                   <span id="total-price">この商品金額：{totalPrice} 円(税抜)</span>
                 </div>
               </div>
             </div>
-            <div class="row">
-              <div class="col-xs-offset-2 col-xs-3">
-                <div class="form-group">
+            <div className="row">
+              <div className="col-xs-offset-2 col-xs-3">
+                <div className="form-group">
                   <p>
-                    <input class="form-control btn btn-warning btn-block" type="submit" value="カートに入れる"/>
+                    <input className="form-control btn btn-warning btn-block" type="submit" value="カートに入れる"/>
                   </p>
 
                 </div>
