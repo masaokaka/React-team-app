6e182ea3c532404b8a30fc00be1a29549ec7c1fe
masaokaka <osushiiisensu@gmail.com>
2021-05-31 11:54:27 +0900

    変更

--- src/components/cart/SumPrice.js
+++ src/components/cart/SumPrice.js
@@ -48,10 +48,10 @@ export const SumPrice = (props) => {
   return (
     <Grid container item cpacing={0} direction="column">
       <Grid container item justify="center">
-        <h3>合計金額(税込)：{(totalPrice * 1.1).toLocaleString()}円</h3>
+        <h3>合計金額(税込)：{Math.floor(totalPrice * 1.1).toLocaleString()}円</h3>
       </Grid>
       <Grid container item justify="center">
-        <h3>内消費税：{(totalPrice * 0.1).toLocaleString()}円</h3>
+        <h3>内消費税：{Math.floor(totalPrice * 0.1).toLocaleString()}円</h3>
       </Grid>
       <Grid container item justify="center">
         <Button variant="contained" onClick={proceedToOrder}>
