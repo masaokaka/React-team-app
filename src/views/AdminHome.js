b97f04ebb8c473056b8c3327f695772a3a442626
masaokaka <osushiiisensu@gmail.com>
2021-05-31 09:55:37 +0900

    ルーティングを変更

--- src/views/AdminHome.js
+++ src/views/AdminHome.js
@@ -7,6 +7,7 @@ import {
   Switch,
   Route,
   Link,
+  useRouteMatch,
 } from "react-router-dom";
 import { ADMIN_ID } from "../status/index";
 import { AdminItems } from "../components/admins/AdminItems";
@@ -16,7 +17,8 @@ import { AdminToppings } from "../components/admins/AdminToppings";
 export const AdminHome = () => {
   const user = useSelector((state) => state.user);
   const history = useHistory();
-  const { adminId } = useParams();
+  const match = useRouteMatch();
+
   //マウント時にユーザーがアドミンではなかった場合にはアクセス拒否
   useEffect(() => {
     if (user.uid !== ADMIN_ID) {
@@ -27,27 +29,21 @@ export const AdminHome = () => {
   return (
     <React.Fragment>
       <Router>
+        <div>
           <div>
-            <div>
-              <Link to={`/admin/users`}>ユーザー情報</Link>
-              <Link to={`/admin/items`}>アイテム情報</Link>
-              <Link to={`/admin/toppings`}>トッピング情報</Link>
-            </div>
-            <Switch>
-              <Route
-                path={`/admin/users`}
-                component={AdminUsers}
-              ></Route>
-              <Route
-                path={`/admin/items`}
-                component={AdminItems}
-              ></Route>
-              <Route
-                path={`/admin/toppings`}
-                component={AdminToppings}
-              ></Route>
-            </Switch>
+            <Link to={`${match.url}/users`}>ユーザー情報</Link>
+            <Link to={`${match.url}/items`}>アイテム情報</Link>
+            <Link to={`${match.url}/toppings`}>トッピング情報</Link>
           </div>
+          <Switch>
+            <Route path={`${match.path}/users`} component={AdminUsers}></Route>
+            <Route path={`${match.path}/items`} component={AdminItems}></Route>
+            <Route
+              path={`${match.path}/toppings`}
+              component={AdminToppings}
+            ></Route>
+          </Switch>
+        </div>
       </Router>
     </React.Fragment>
   );
