b05156ca4d5f7aa62492791e2278e52fa6010440
OkawaraTakumi <takumi.okawara@rakus-partners.co.jp>
2021-06-02 12:23:16 +0900

    '変更'

--- src/components/home/Slider.js
+++ src/components/home/Slider.js
@@ -3,4 +3,11 @@ import React, { useState, useEffect } from "react";
 
 export const Slider = (props) =>{
     
+
+    return (
+        <React.Fragment>
+                <div></div>
+
+        </React.Fragment>
+    )
 }
